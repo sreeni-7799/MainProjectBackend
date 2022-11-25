795e6712c71efb75113260405c5a0aae7ab7df6e
FadiaHaris123 <fadia.haris@experionglobal.com>
2022-11-25 12:00:09 +0530

    changes added

--- src/main/java/com/experion/mainbackend/dao/ProductRepo.java
+++ src/main/java/com/experion/mainbackend/dao/ProductRepo.java
@@ -1,6 +1,7 @@
 package com.experion.mainbackend.dao;
 
 import com.experion.mainbackend.entity.Product;
+import com.experion.mainbackend.entity.UserRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,4 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "product",path = "managers")
 public interface ProductRepo extends JpaRepository<Product,Long> {
 
+    @CrossOrigin(origins = "*")
+    @RepositoryRestResource(collectionResourceRel = "userprofile",path="user_profile")
+    interface UserRegistrationRepo extends JpaRepository<UserRegistration,Long> {
+    }
 }
